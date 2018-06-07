4986ed348bc2ffc27474aa21167240a6dd1bb302
Darwin Sardual <darwin.sardual@gmail.com>
2018-06-07 18:55:05 +0800

    test log

--- src/components/Main.js
+++ src/components/Main.js
@@ -179,7 +179,8 @@ class Main extends Component{
       response.json().then(function(data){
         // we expect a json object
         tempThis.setState({settings: {apiKey: data.apiKey}}, ()=>{
-          console.log(this.state.settings.apiKey);
+          //console.log(this.state.settings.apiKey);
+          console.log('darwin');
         });
       })
     });
